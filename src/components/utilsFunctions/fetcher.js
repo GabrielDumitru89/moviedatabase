b76d760614c23144896f46be4e8376044b559c0a
ToDy95 <todyimmortal@gmail.com>
2023-11-29 20:22:38 +0200

    init react basic template

---
+++ src/components/utilsFunctions/fetcher.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import axios from 'axios';
+const fetcher = (params) => {
+  const options = {
+    method: 'GET',
+    url: `${process.env.REACT_APP_BASE_URL}${params}`,
+    headers: {
+      'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
+      'X-RapidAPI-Host': `${process.env.REACT_APP_API_HOST}`
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    if (response.ok) {
+      return response
+    } else {
+      return null
+    }
+  } catch (error) {
+    throw new Error("Something is wrong!!!!")
+  }
+
+}
+
+export default fetcher
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom'
+import ModalComponent from './components/utilsView/Modal';
+import Context from './components/context/Context';
+import Toast from './components/utilsView/Toast';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Context>
+    <BrowserRouter>
+      <App />
+      <Toast />
+      <ModalComponent />
+    </BrowserRouter>
+  </Context>
 );
 
 // If you want to start measuring performance in your app, pass a function
